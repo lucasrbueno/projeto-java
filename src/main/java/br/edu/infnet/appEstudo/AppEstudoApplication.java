033ffa83ca782dd901660e11863be591b5faa2d1
lucasrbueno <lucasbueno2@hotmail.com>
2021-01-30 14:43:27 -0300

    Aula01

---
+++ src/main/java/br/edu/infnet/appEstudo/AppEstudoApplication.java
@@ -0,0 +1,13 @@
+package br.edu.infnet.appEstudo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AppEstudoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AppEstudoApplication.class, args);
+	}
+
+}
---
+++ src/test/java/br/edu/infnet/appEstudo/AppEstudoApplicationTests.java
@@ -0,0 +1,13 @@
+package br.edu.infnet.appEstudo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AppEstudoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
